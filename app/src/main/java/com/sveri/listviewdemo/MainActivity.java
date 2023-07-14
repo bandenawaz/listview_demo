fd5a38a8531f7f9ac41db8f83ee402d404c7b6ba
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-07-14 12:15:13 +0530

    Final App

---
+++ app/src/main/java/com/sveri/listviewdemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.sveri.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] prg = {"C","C++","Java","Python","C#","PHP"};
+    private ListView lv;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,prg);
+        lv = findViewById(R.id.listPrgLng);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this, "You clicked : "+prg[position],
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
